9ca4e8f2a48c01bcf92c10f1a783dfcf2035d54e
rishabh <rish100abh@gmail.com>
2026-04-29 23:34:52 +0530

    fix cors.ts

--- server/src/config/cors.ts
+++ server/src/config/cors.ts
@@ -1,6 +1,19 @@
 import { env } from "./env.js";
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://project1-client-pq4r.vercel.app",
+];
+
 export const corsOptions = {
-  origin: env.clientUrl,
+  origin: (origin: string | undefined, callback: any) => {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+
+    return callback(new Error("Not allowed by CORS"));
+  },
   credentials: true,
 };
