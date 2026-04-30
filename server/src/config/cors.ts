5b3635655055080f02c49b415a74237bb749ed68
rishabh <rish100abh@gmail.com>
2026-04-30 11:14:52 +0530

    fix cors.ts

--- server/src/config/cors.ts
+++ server/src/config/cors.ts
@@ -1,18 +1,7 @@
-import { env } from "./env.js";
-
-const allowedOrigins = [
-  env.clientUrl,                 // production
-  "http://localhost:5173",       // dev (vite)
-  "https://project1-client-59pj-705aisjet-rishabh-yadavs-projects-02177f0b.vercel.app"       // optional
-];
+import cors from "cors";
+import { env } from "./env";
 
 export const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: env.clientUrl,
   credentials: true,
 };
