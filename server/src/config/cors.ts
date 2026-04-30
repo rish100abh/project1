e5b241be40e474bdc2c5ecc5cbf82d2159129afd
rishabh <rish100abh@gmail.com>
2026-04-30 15:20:16 +0530

    fix cors.ts

--- server/src/config/cors.ts
+++ server/src/config/cors.ts
@@ -4,6 +4,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   env.clientUrl,
   "https://project1-client-59jp.vercel.app",
+   "http://localhost:3000",
+  "https://project1-client-59pj-3knz1u0ou-rishabh-yadavs-projects-02177f0b.vercel.app",
 ].filter(Boolean);
 
 export const corsOptions = {
